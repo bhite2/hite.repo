a81509fce0fbe03261aec2bc709562e0beabf382
Bryan Hite <bhite2@mail.usf.edu>
2022-11-05 16:19:52 -0400

    Added Destinations

---
+++ daytripgen.py
@@ -0,0 +1,9 @@
+destinations = ['Miami,FL', 'New York City, NY', 'Los Angeles, CA', 'Atlanta, GA', 'Dallas,TX']
+
+restaurants = ["Ruth's Chris Steakhouse", 'Ocean Prime', "Shula's", 'Seasons 52', 'Flemings']
+
+entertainment = ['Broadway Show', 'Museum', 'Theme Park', 'Sporting Event', 'Night Club']
+
+transportation = ['Car', 'Bus', 'Train', 'Plane', 'Boat']
+
+def destination_gen(random.choice(destinations))
